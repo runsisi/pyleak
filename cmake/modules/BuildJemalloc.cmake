8e37bcca478ebd39660933737ac6c92ea0b47cd4
runsisi <runsisi@hust.edu.cn>
2019-08-27 20:06:45 +0800

    add mempool from ceph
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- cmake/modules/BuildJemalloc.cmake
+++ cmake/modules/BuildJemalloc.cmake
@@ -7,7 +7,7 @@ set(jemalloc_root_dir "${CMAKE_BINARY_DIR}/jemalloc")
 set(jemalloc_url "https://github.com/jemalloc/jemalloc/releases/download/5.2.1/jemalloc-5.2.1.tar.bz2")
 set(jemalloc_url_hash "SHA256=34330e5ce276099e2e8950d9335db5a875689a4c6a56751ef3b1d8c537f887f6")
 set(jemalloc_config_args "--disable-initial-exec-tls")
-list(APPEND jemalloc_config_args "--without-export")
+#list(APPEND jemalloc_config_args "--without-export")
 list(APPEND jemalloc_config_args "--enable-debug")
 
 include(ExternalProject)
@@ -29,3 +29,4 @@ set(jemalloc_INCLUDE_DIRS
 
 set(jemalloc_LIBRARIES
   ${jemalloc_root_dir}/src/jemalloc/lib/libjemalloc_pic.a)
+#  ${jemalloc_root_dir}/src/jemalloc/lib/libjemalloc.so)
