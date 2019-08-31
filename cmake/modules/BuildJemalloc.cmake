081373f162c4689431e7157a472365c2314c9d7e
runsisi <runsisi@hust.edu.cn>
2019-08-31 21:31:46 +0800

    cmake/module: build jemalloc
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- cmake/modules/BuildJemalloc.cmake
+++ cmake/modules/BuildJemalloc.cmake
@@ -1,32 +1,66 @@
-##
-# Make file for jemalloc project
-##
+#
+# CMake file for jemalloc
+#
 
-# v5.2.1
-set(jemalloc_root_dir "${CMAKE_BINARY_DIR}/jemalloc")
-set(jemalloc_url "https://github.com/jemalloc/jemalloc/releases/download/5.2.1/jemalloc-5.2.1.tar.bz2")
-set(jemalloc_url_hash "SHA256=34330e5ce276099e2e8950d9335db5a875689a4c6a56751ef3b1d8c537f887f6")
-set(jemalloc_config_args "--disable-initial-exec-tls")
-#list(APPEND jemalloc_config_args "--without-export")
-list(APPEND jemalloc_config_args "--enable-debug")
+function(do_build_jemalloc)
+  set(jemalloc_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/jemalloc)
+  set(jemalloc_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/jemalloc)
+  set(jemalloc_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/jemalloc/artifacts)
 
-include(ExternalProject)
-ExternalProject_Add(jemalloc
-  URL ${jemalloc_url}
-  URL_HASH ${jemalloc_url_hash}
-  CONFIGURE_COMMAND ${jemalloc_env} ./configure ${jemalloc_config_args}
+  list(APPEND jemalloc_CONFIG_ARGS --prefix=${jemalloc_INSTALL_DIR})
+  list(APPEND jemalloc_CONFIG_ARGS --disable-initial-exec-tls)
+  #list(APPEND jemalloc_CONFIG_ARGS --disable-cxx)
+  #list(APPEND jemalloc_CONFIG_ARGS --without-export)
+  list(APPEND jemalloc_CONFIG_ARGS --enable-debug)
+  #list(APPEND jemalloc_CONFIG_ARGS --with-jemalloc-prefix=xxx_)
 
-# Temporarily forcing single thread as multi-threaded make is causing build
-# failues.
-  BUILD_COMMAND make -j1
-  BUILD_IN_SOURCE 1
-  INSTALL_COMMAND ""
-  TEST_COMMAND ""
-  PREFIX ${jemalloc_root_dir})
+  if(NOT EXISTS ${jemalloc_SOURCE_DIR}/configure)
+    execute_process(
+      COMMAND ./autogen.sh
+      WORKING_DIRECTORY ${jemalloc_SOURCE_DIR}
+    )
+  endif()
 
-set(jemalloc_INCLUDE_DIRS
-  ${jemalloc_root_dir}/src/jemalloc/include)
+  include(ExternalProject)
+  ExternalProject_Add(jemalloc-ext
+    PREFIX ${jemalloc_BINARY_DIR}
+    SOURCE_DIR ${jemalloc_SOURCE_DIR}
+    CONFIGURE_COMMAND CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER}
+        <SOURCE_DIR>/configure ${jemalloc_CONFIG_ARGS}
+    BINARY_DIR ${jemalloc_BINARY_DIR}
+    BUILD_COMMAND $(MAKE)
+    BUILD_ALWAYS TRUE
+    BUILD_BYPRODUCTS ${jemalloc_LIBRARY}
+    INSTALL_DIR ${jemalloc_INSTALL_DIR}
+    INSTALL_COMMAND env -i $(MAKE) install
+    TEST_COMMAND true
+  )
+endfunction()
 
-set(jemalloc_LIBRARIES
-#  ${jemalloc_root_dir}/src/jemalloc/lib/libjemalloc_pic.a)
-  ${jemalloc_root_dir}/src/jemalloc/lib/libjemalloc.so)
+macro(build_jemalloc)
+  do_build_jemalloc()
+
+  ExternalProject_Get_Property(jemalloc-ext INSTALL_DIR)
+
+  set(JEMALLOC_INCLUDE_DIR ${INSTALL_DIR}/include)
+  file(MAKE_DIRECTORY ${JEMALLOC_INCLUDE_DIR})
+  set(JEMALLOC_STATIC_LIBRARY jemalloc-static)
+  set(JEMALLOC_SHARED_LIBRARY jemalloc-shared)
+
+  add_library(jemalloc-static STATIC IMPORTED)
+  add_dependencies(jemalloc-static jemalloc-ext)
+
+  set_target_properties(jemalloc-static PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES ${JEMALLOC_INCLUDE_DIR}
+    IMPORTED_LINK_INTERFACE_LANGUAGES "C;CXX"
+    IMPORTED_LOCATION ${INSTALL_DIR}/lib/libjemalloc_pic.a
+  )
+
+  add_library(jemalloc-shared SHARED IMPORTED)
+  add_dependencies(jemalloc-shared jemalloc-ext)
+
+  set_target_properties(jemalloc-shared PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES ${JEMALLOC_INCLUDE_DIR}
+    IMPORTED_LOCATION ${INSTALL_DIR}/lib/libjemalloc.so
+  )
+endmacro()
