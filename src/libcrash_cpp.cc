dcb05f6365e37721bd6a39b3e87a423f82d61d91
runsisi <runsisi@hust.edu.cn>
2019-09-04 09:38:20 +0800

    dynamic link libcrash, symbol still resolves from main program
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

--- src/libcrash_cpp.cc
+++ src/libcrash_cpp.cc
@@ -8,11 +8,15 @@
 #define EXPORT __attribute__((visibility("default")))
 
 #include <iostream>
+//#include <memory>
 
 using namespace std;
 
 extern "C" EXPORT
 void libcrash_init() {
+//  int* cp = (int*)std::malloc(sizeof(int));
+//  std::free(cp);
+
   int* p = new int;
   *p = 0;
   delete p;
--- src/test_crash_cpp.cc
+++ src/test_crash_cpp.cc
@@ -8,7 +8,9 @@
 #include <memory>
 #include <dlfcn.h>
 
-typedef void (*init_t)();
+//typedef void (*init_t)();
+
+extern "C" void libcrash_init();
 
 int main() {
   // comment out the following three lines will crash the app
@@ -19,21 +21,23 @@ int main() {
   void* ptr = std::malloc(sizeof(int));
   std::free(ptr);
 
-  void *handle = dlopen("./libcrash_cpp.so", RTLD_LAZY);
-  if (!handle) {
-    char* err = dlerror();
-    return -1;
-  }
+//  void *handle = dlopen("./libcrash_cpp.so", RTLD_LAZY);
+//  if (!handle) {
+//    char* err = dlerror();
+//    return -1;
+//  }
+//
+//  init_t init = (init_t)dlsym(handle, "libcrash_init");
+//  if (!init) {
+//    char* err = dlerror();
+//    return -1;
+//  }
+//
+//  init();
+//
+//  dlclose(handle);
 
-  init_t init = (init_t)dlsym(handle, "libcrash_init");
-  if (!init) {
-    char* err = dlerror();
-    return -1;
-  }
-
-  init();
-
-  dlclose(handle);
+  libcrash_init();
 
   return 0;
 }
